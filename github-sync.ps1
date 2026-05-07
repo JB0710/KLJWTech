5b6388087e6d1e4d24ccee078342d6f8d67c6c00
Jonathan Wood <jonathan.wood@kljwtech.dev>
2026-05-07 20:10:15 +0000

    May 7, 2026, 3:10 PM

--- github-sync.ps1
+++ github-sync.ps1
@@ -17,7 +17,7 @@ cd D:/GitHub/NOCpinger
 git init
 #echo "# KLJWTech" >> README.md
 git config --global --add safe.directory D:/GitHub/NOCpinger
-git add README.md
+#git add README.md
 git branch -M main
 git remote add origin https://github.com/JB0710/NOC-Dashboard-NOCpinger-.git
 git config set advice.addEmptyPathspec false
