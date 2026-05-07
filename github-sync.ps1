d06ee62cd1161a3d86c3dbda91764d0407aa711a
Jonathan Wood <jonathan.wood@kljwtech.dev>
2026-05-07 00:49:50 +0000

    May 6, 2026, 7:49 PM

---
+++ github-sync.ps1
@@ -0,0 +1,13 @@
+cd LocalRepo
+git init
+echo "# KLJWTech" >> README.md
+git config --global --add safe.directory D:/GitHub/LocalRepo
+git add README.md
+git branch -M main
+git remote add origin https://github.com/JB0710/KLJWTech.git
+git config set advice.addEmptyPathspec false
+git add
+git config --global user.email "jonathan.wood@kljwtech.dev"
+git config --global user.name "Jonathan Wood"
+git commit -m "Initial commit"
+git push -u origin main
