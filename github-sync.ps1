531051d125e236ea86c4efb4b63278df0d61a491
Jonathan Wood <jonathan.wood@kljwtech.dev>
2026-05-07 20:08:30 +0000

    May 7, 2026, 3:08 PM

--- github-sync.ps1
+++ github-sync.ps1
@@ -1,10 +1,25 @@
-cd LocalRepo
+cd D:/GitHub/LocalRepo
 git init
 echo "# KLJWTech" >> README.md
 git config --global --add safe.directory D:/GitHub/LocalRepo
 git add README.md
 git branch -M main
 git remote add origin https://github.com/JB0710/KLJWTech.git
+git config set advice.addEmptyPathspec false
+git add
+git config --global user.email "jonathan.wood@kljwtech.dev"
+git config --global user.name "Jonathan Wood"
+git commit -m "Initial commit"
+git push -u origin main
+
+
+cd D:/GitHub/NOCpinger
+git init
+echo "# KLJWTech" >> README.md
+git config --global --add safe.directory D:/GitHub/NOCpinger
+git add README.md
+git branch -M main
+git remote add origin https://github.com/JB0710/NOC-Dashboard-NOCpinger-.git
 git config set advice.addEmptyPathspec false
 git add
 git config --global user.email "jonathan.wood@kljwtech.dev"
